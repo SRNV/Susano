bec15d8b53c570f18f32e4b0485244574fb6037d
SRNV <alula.rudy@gmail.com>
2021-04-28 10:11:44 +0200

    avoid chmodSync on all files, fix regexp, save once path to test

--- mod.ts
+++ mod.ts
@@ -1,51 +1,49 @@
 import { existsSync } from "./deps/exists.ts";
 import { join } from "./deps/path.ts";
 
-const stressMap: string[] = [];
+const testFiles: string[] = [];
 
 interface StressOptions {
   root: string;
 }
 function read(path: string): void {
-  if (!existsSync(path)) return;
-  if (Deno.build.os !== "windows") {
-    Deno.chmodSync(path, 0o777);
-  }
-  const stats = Deno.statSync(path);
-  if (stats.isFile) {
-    console.warn(`Stress mode: ${path}`);
-    stressMap.push(path);
-  }
-  if (stats.isDirectory) {
-    if (Deno.build.os !== "windows") {
-      Deno.chmodSync(path, 0o777);
+  try {
+    if (!existsSync(path)) return;
+    const stats = Deno.statSync(path);
+    if (stats.isFile && !testFiles.includes(path)) {
+      testFiles.push(path);
     }
-    const dir = Deno.readDirSync(path);
-    for (const dirEntry of dir) {
-      const { name } = dirEntry;
-      const subpath = join(path, name);
-      if (name.match(/((_|.)test.(js|ts))$/i)) {
-        read(subpath);
-      } else {
-        if (Deno.build.os !== "windows") {
-          Deno.chmodSync(subpath, 0o777);
-        }
-        const stats = Deno.statSync(subpath);
-        if (stats.isDirectory) {
+    if (stats.isDirectory) {
+      const dir = Deno.readDirSync(path);
+      for (const dirEntry of dir) {
+        const { name } = dirEntry;
+        const subpath = join(path, name);
+        if (name.match(/((_|\.)test\.(js|ts))$/i)) {
           read(subpath);
+        } else {
+          const stats = Deno.statSync(subpath);
+          if (stats.isDirectory) {
+            read(subpath);
+          }
         }
       }
     }
+  } catch(err) {
+    console.error(err);
+    throw new Error('[Susano] exit');
   }
 }
 async function runTests() {
-  console.warn("[Susano] Stress mode: running.");
-  if (stressMap.length > 1) {
+  console.warn("[Susano]\trunning.");
+  if (testFiles.length === 0) {
+    console.warn('[Susano]\tno test found')
+  }
+  if (testFiles.length > 1) {
     Deno.run({
       cmd: ["deno", "test", "--failfast", "--unstable", "-A"],
     });
   } else {
-    stressMap.forEach((path) => {
+    testFiles.forEach((path) => {
       Deno.run({
         cmd: ["deno", "test", "--failfast", "--unstable", "-A", path],
       });
@@ -53,26 +51,31 @@ async function runTests() {
   }
 }
 export async function stressMode(opts: StressOptions) {
-  const status = await Deno.permissions.query({ name: "read" });
-  if (status.state !== "granted") {
-    console.error("[Susano] need read permission");
+  const readPermission = await Deno.permissions.query({ name: "read" });
+  const runPermission = await Deno.permissions.query({ name: "run" });
+  if (readPermission.state !== "granted") {
+    console.error("[Susano]\t read permission isn't granted");
+    Deno.exit(0);
+  }
+  if (runPermission.state !== "granted") {
+    console.error("[Susano]\t run permission isn't granted");
     Deno.exit(0);
   }
   if (existsSync(opts.root)) {
     const stress = Deno.watchFs(Deno.cwd());
-    console.warn(`[Susano] Stress mode enabled: using ${opts.root}`);
+    console.warn(`[Susano]\tStress mode enabled: using ${opts.root}`);
     read(opts.root);
     runTests();
     for await (const event of stress) {
       const { kind } = event;
       if (kind === "access") {
-        console.log("[Susano] Stress mode: ", ...event.paths);
+        read(opts.root);
         runTests();
       }
     }
   } else {
     console.error(
-      `[Susano] ${opts.root} is not a file or directory. please specify in options a existing file or directory.`,
+      `[Susano]\t${opts.root} is not a file or directory. please specify in options a existing file or directory.`,
     );
   }
 }
